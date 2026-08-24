ff7294b7c1a6b3fb0f9e3bd9851a9e56634ff557
Aragorn-untold <idytkoni@gmail.com>
2026-08-24 10:35:20 +0000

    solved

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+import pytest
+from app import main
+
+
+def test_sell_with_float(monkeypatch) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.20)
+    assert main.cryptocurrency_action(1.50) == "Buy more cryptocurrency"
+
+
+def test_buy_with_int(monkeypatch) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1)
+    assert main.cryptocurrency_action(0.8) == "Sell all your cryptocurrency"
+
+
+def test_do_nothing(monkeypatch) -> None:
+    monkeypatch.setattr(main, "get_exchange_rate_prediction", lambda x: 1.25)
+    assert main.cryptocurrency_action(1.25) == "Do nothing"
